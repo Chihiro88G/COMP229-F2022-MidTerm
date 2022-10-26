24bf23fab1d6c8916346010f3990fa003f6e92be
Chihiro Hasegawa <1114chihiro@gmail.com>
2022-10-26 19:00:18 -0400

    1st commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://chihiro:UvLzpldvBOJuWGak@mongodbserver.utqfcxo.mongodb.net/book_collection?retryWrites=true&w=majority"
+};
